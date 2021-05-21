242f56ffbc859607e74bc2781e97815d39642584
theAndalorian <andymcd2000@gmail.com>
2021-05-21 13:54:20 -0700

    Board Class Update
    changed toString method to match form of server board. Added OutOfBounds value and restrictions to set/get tile methods in order to deal with edge of board.

--- src/main/java/ubc/cosc322/Board.java
+++ src/main/java/ubc/cosc322/Board.java
@@ -6,26 +6,38 @@ public class Board {
 	public static final int BLACK = 1;
 	public static final int WHITE = 2;
 	public static final int ARROW = 3;
+	public static final int OUTOFBOUNDS = 4;
 	
 	// fields
-	private int[][] tiles;
+	private int[][] tiles; // tiles[xPos][yPos], 0 row&col are set to 0(EMPTY) but never used
 	
 	// constructors
 	public Board() {
 		this.tiles = new int[11][11];
-		for (int i=1; i<11; i++) {
-			for (int j=1; j<11; j++) {
+		for (int j=0; j<11; j++) {
+			for (int i=0; i<11; i++) {
 				this.tiles[i][j] = Board.EMPTY;
 			}
 		}
 	}
 	
 	// methods
-	public void setTile(int newOccupent, int xPos, int yPos) {
-		this.tiles[xPos][yPos] = newOccupent;
+	public void setTile(int newOccupent, int x, int y) {
+		// check for invalid tile
+		if (x<1 || x>11 || y<1 || y>11) {
+			// System.out.println("Can't set this tile");
+			return;
+		}
+		this.tiles[x][y] = newOccupent;
 	}
 	
 	public int getTile(int x, int y) {
+		// check for invalid tile
+		if (x<1 || x>11 || y<1 || y>11) {
+			// System.out.println("Can't get this tile");
+			return Board.OUTOFBOUNDS;
+		}
+		// return tile value
 		return this.tiles[x][y];
 	}
 	
@@ -58,8 +70,8 @@ public class Board {
 	
 	public String toString() {
 		String output = "Current Board: \n";
-		for (int i=1; i<11; i++) {
-			for (int j=1; j<11; j++) {
+		for (int j=11; j>=1; j--) {
+			for (int i=1; i<11; i++) {
 				switch (this.tiles[i][j]) {
 				case Board.EMPTY: output += "   "; break;
 				case Board.BLACK: output += " B "; break;
